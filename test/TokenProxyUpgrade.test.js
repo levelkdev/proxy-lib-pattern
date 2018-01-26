0c6f2b3678889bf02bc48c51bc57393db675ef30
Mike Calvanese <mfcalvanese@gmail.com>
2018-01-26 14:30:48 -0500

    name change

--- test/TokenProxyUpgrade.test.js
+++ test/TokenProxyUpgrade.test.js
@@ -12,34 +12,38 @@ const TokenProxy = artifacts.require('TokenProxy')
 describe('TokenProxyUpgrade', () => {
   it.only('token proxy upgrade', async () => {
     const tokenProxy = await TokenProxy.new()
-    const tokenV0Proxy = await TokenV0.new()
-    await tokenProxy.upgradeTo('v0', tokenV0Proxy.address)
+    const tokenV0 = await TokenV0.new()
+    await tokenProxy.upgradeTo('v0', tokenV0.address)
 
-    const tokenV0 = await TokenV0.at(tokenProxy.address)
-    console.log('_totalSupply: ', (await tokenV0.totalSupply()).toNumber())
+    const tokenProxyV0 = await TokenV0.at(tokenProxy.address)
+    console.log('_totalSupply: ', (await tokenProxyV0.totalSupply()).toNumber())
 
     console.log('init()')
-    await tokenV0.init()
+    await tokenProxyV0.init()
 
-    console.log('_totalSupply: ', (await tokenV0.totalSupply()).toNumber())
-    console.log(`balanceOf(${accounts[0]}): `, (await tokenV0.balanceOf(accounts[0])).toNumber())
+    console.log('_totalSupply: ', (await tokenProxyV0.totalSupply()).toNumber())
+    console.log(`balanceOf(${accounts[0]}): `, (await tokenProxyV0.balanceOf(accounts[0])).toNumber())
 
     // this will still be 0...
-    console.log('TOKEN V0 PROXY _totalSupply: ', ((await tokenV0Proxy.totalSupply()).toNumber()))
+    console.log('TOKEN V0 _totalSupply: ', ((await tokenV0.totalSupply()).toNumber()))
     console.log('')
 
     console.log('upgrade to v1 mintable')
-    const tokenV1Proxy = await TokenV1.new()
-    await tokenProxy.upgradeTo('v1', tokenV1Proxy.address)
-    const tokenV1 = await TokenV1.at(tokenProxy.address)
+    const tokenV1 = await TokenV1.new()
+    await tokenProxy.upgradeTo('v1', tokenV1.address)
+    const tokenProxyV1 = await TokenV1.at(tokenProxy.address)
 
     const amt = 2345678
     console.log(`mint ${amt} to ${accounts[1]}`)
-    await tokenV1.mint(accounts[1], amt)
+    await tokenProxyV1.mint(accounts[1], amt)
 
-    console.log('_totalSupply: ', (await tokenV1.totalSupply()).toNumber())
-    console.log(`balanceOf(${accounts[0]}): `, (await tokenV1.balanceOf(accounts[0])).toNumber())
-    console.log(`balanceOf(${accounts[1]}): `, (await tokenV1.balanceOf(accounts[1])).toNumber())
-    console.log('TOKEN V1 PROXY _totalSupply: ', ((await tokenV1Proxy.totalSupply()).toNumber()))
+    console.log('_totalSupply: ', (await tokenProxyV1.totalSupply()).toNumber())
+    console.log(`balanceOf(${accounts[0]}): `, (await tokenProxyV1.balanceOf(accounts[0])).toNumber())
+    console.log(`balanceOf(${accounts[1]}): `, (await tokenProxyV1.balanceOf(accounts[1])).toNumber())
+
+
+
+    console.log('TOKEN V0 _totalSupply: ', ((await tokenV0.totalSupply()).toNumber()))
+    console.log('TOKEN V1 _totalSupply: ', ((await tokenV1.totalSupply()).toNumber()))
   })
 })
